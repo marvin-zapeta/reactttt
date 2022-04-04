ef18590b85be8ed33215951a2cb0f2d1b9c861b6
marvin-zapeta <90151997+marvin-zapeta@users.noreply.github.com>
2022-04-03 22:03:51 -0600

    terminadooooo

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Saludo from "./components/Saludo";
+import Comentario from "./components/Comentario"; 
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container mt-5">
+     <h1>proyecto desde cero</h1>
+     <Saludo persona='Ignacio' edad={30}/>
+     <Saludo persona='Juanito' edad={25}/>
+     <Saludo persona='Pedrito' edad={33}/>
+     <hr />
+     <h3>cajita de comentarios</h3>
+     <Comentario
+        urlImagen='https://picsum.photos/64'
+        persona='Ignacio'
+        texto='ddddddddddddd'
+     />
+     <Comentario 
+        urlImagen='https://picsum.photos/64'
+        persona='Juanito'
+        texto='aaaaaaaaaaaaaaaaa'
+     />
+     <Comentario
+      urlImagen='https://picsum.photos/64'
+      persona='Pedrito'
+      texto='ffffffffffffffff'     
+     />
     </div>
   );
 }
